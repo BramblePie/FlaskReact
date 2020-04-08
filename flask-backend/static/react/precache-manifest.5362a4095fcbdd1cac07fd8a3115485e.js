8d3385524481ffd1eb028a52981b1ddbaa22d56e
Bram Pieters <s1110012@student.windesheim.nl>
2020-04-08 23:24:16 +0200

    Change back to func App

--- flask-backend/static/react/precache-manifest.5362a4095fcbdd1cac07fd8a3115485e.js
+++ flask-backend/static/react/precache-manifest.5362a4095fcbdd1cac07fd8a3115485e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb1e4856b2979e0efc1f9402376b5864",
+    "revision": "1ac1e58c0f02750ed21f6bd81280a72f",
     "url": "/static/react/../../templates/index.html"
   },
   {
-    "revision": "c2e42f4eac6b0aa61673",
+    "revision": "dc39f753f09376e73ef7",
     "url": "/static/react/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "ecc967eec98dcd1e0c01",
-    "url": "/static/react/js/2.5a9d0ba3.chunk.js"
+    "revision": "c3f35d756ed98c3839a3",
+    "url": "/static/react/js/2.aeb10df2.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/react/js/2.5a9d0ba3.chunk.js.LICENSE.txt"
+    "url": "/static/react/js/2.aeb10df2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2e42f4eac6b0aa61673",
-    "url": "/static/react/js/main.42cd0887.chunk.js"
+    "revision": "dc39f753f09376e73ef7",
+    "url": "/static/react/js/main.f2ed76cc.chunk.js"
   },
   {
     "revision": "e7811486d709951790fb",
