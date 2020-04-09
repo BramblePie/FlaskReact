c26b9097aab2483ae5fc4f215b44223a574f618e
Bram Pieters <s1110012@student.windesheim.nl>
2020-04-09 18:34:58 +0200

    Add current file launch config

--- flask-backend/notebooks/data_prep.py
+++ flask-backend/notebooks/data_prep.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 zipfile = "zip://raw-data/wijkenbuurten2019.zip"
 print(f"Loading {zipfile}...")
-data = gpd.read_file(zipfile)
+gdf = gpd.read_file(zipfile)
 print(f"Loaded {zipfile}")
 
-print(data.info())
+print(gdf.head(1))
+print(gdf.info())
