18f37f401affbfc6523b09084043eb87a286171c
Bram Pieters <s1110012@student.windesheim.nl>
2020-04-10 21:47:50 +0200

    Add data loading exception

--- flask-backend/notebooks/data_prep.py
+++ flask-backend/notebooks/data_prep.py
@@ -1,18 +1,22 @@
 import geopandas as gpd
 import pandas as pd
 
-zipfile = "zip://raw-data/wijkenbuurten2019.zip"
-print(f"Loading {zipfile}...")
-gdf = gpd.read_file(zipfile)
-print(f"Loaded {zipfile}")
+try:
+    zipfile = "zip://raw-data/wijkenbuurten2019.zip"
+    print(f"Loading {zipfile}...")
+    gdf = gpd.read_file(zipfile)
+    print(f"Loaded {zipfile}")
 
-print(gdf.head(1))
-print(gdf.info())
+    print(gdf.head(1))
+    print(gdf.info())
 
-inwgem = pd.DataFrame(gdf[["GM_NAAM", "AANT_INW"]])
-inwgem.where(inwgem["AANT_INW"] > 0, inplace=True)
-inwgem = inwgem.groupby(["GM_NAAM"]).sum()
+    inwgem = pd.DataFrame(gdf[["GM_NAAM", "AANT_INW"]])
+    inwgem.where(inwgem["AANT_INW"] > 0, inplace=True)
+    inwgem = inwgem.groupby(["GM_NAAM"]).sum()
 
+    def get_gem_min_inw(aantal):
+        return inwgem[inwgem["AANT_INW"] > aantal].to_dict()
+except:
 
-def get_gem_min_inw(aantal):
-    return inwgem[inwgem["AANT_INW"] > aantal].to_dict()
+    def get_gem_min_inw(aantal):
+        return None
