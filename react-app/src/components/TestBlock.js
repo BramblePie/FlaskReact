8f08d3dad322cb12e6912bd83efcc504473bf017
Bram Pieters <s1110012@student.windesheim.nl>
2020-04-10 21:45:18 +0200

    Revert "Revert "dev""
    This reverts commit f05fac1379f3c4eb529c52528376d97ad8435397.

---
+++ react-app/src/components/TestBlock.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './TestBlock.css'
+
+function TestBlock() {
+
+    return (
+        <div>
+            <h2>TESTING</h2>
+        </div>
+    );
+}
+
+export default TestBlock;
