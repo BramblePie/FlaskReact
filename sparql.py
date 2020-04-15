8008e1ada841ed3aaa8d47efc18b29bfdfec4f7d
32467508 <mdalen.96@gmail.com>
2020-04-15 14:03:19 +0200

    test

--- sparql.py
+++ sparql.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# from pandas.io.json import json_normalize
+from pandas.io.json import json_normalize
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def query_kadaster(sparql_query, sparql_service_url):
@@ -9,16 +9,16 @@ def query_kadaster(sparql_query, sparql_service_url):
     result = sparql.query().convert()
     return pd.json_normalize(result["results"]["bindings"])
 
-sparql_query = """
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX brt: <http://brt.basisregistraties.overheid.nl/def/top10nl#>
-PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-SELECT  ?geo (?x as ?geoLabel) ?naam WHERE {
- ?x a brt:Ziekenhuis;
- 	brt:naam ?naam;
-    geo:hasGeometry/geo:asWKT ?geo.
-}
-"""
-sparql_service_url = "https://data.pdok.nl/sparql"
+# sparql_query = """
+# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+# PREFIX brt: <http://brt.basisregistraties.overheid.nl/def/top10nl#>
+# PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+# SELECT  ?geo (?x as ?geoLabel) ?naam WHERE {
+#  ?x a brt:Ziekenhuis;
+#  	brt:naam ?naam;
+#     geo:hasGeometry/geo:asWKT ?geo.
+# }
+# """
+# sparql_service_url = "https://data.pdok.nl/sparql"
 
-data = query_kadaster(sparql_query, sparql_service_url)
+# data = query_kadaster(sparql_query, sparql_service_url)
