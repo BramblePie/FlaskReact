94f03f9f4bf616ddebbf31749097f9c0b8f0954b
32467508 <mdalen.96@gmail.com>
2020-05-07 12:03:45 +0200

    commit

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, Blueprint, request
 from flask_restplus import Api, Resource
 
+from demografie import demografieAPI
+
 # from notebooks.data_prep import get_gem_min_inw
 import notebooks.data_prep as notebook
 
@@ -32,9 +34,14 @@ class GetNumber(Resource):
         random.seed(seed)
         return {seed: math.floor(random.uniform(0, 10))}
 
-
 @api.route("/gemeente/<int:inwoners>")
 class AantalInwoners(Resource):
     def get(self, inwoners):
         """Get all gemeentes met minimaal zoveel inwoners"""
         return notebook.get_gem_min_inw(inwoners)
+
+@api.route("/demografie/<string:text>")
+class DemografieFrame(Resource):
+    def get(self, text):
+        """Test functie demografie"""
+        return demografieAPI(text)
